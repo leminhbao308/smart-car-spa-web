330ffe03f3dc80f0f1a02db95a8c85f932827d59
NAMLEHOANG\lehoa <lehoangnam.31123@gmail.com>
2025-09-12 15:37:30 +0700

    style(app): configure AntdRegistry and patch for antd in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
+import "@ant-design/v5-patch-for-react-19";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Smart Car Spa",
@@ -24,8 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body>
+        <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
   );
