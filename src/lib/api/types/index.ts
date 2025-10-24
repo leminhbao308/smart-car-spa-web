5af91c82421c27f488e0ae67de4c650f3b0b482d
Lê Minh Bảo <besilent308@gmail.com>
2025-10-25 04:09:36 +0700

    feat(modals): add ExcelImportModal and update exports for consistency

--- src/lib/api/types/index.ts
+++ src/lib/api/types/index.ts
@@ -3,69 +3,75 @@
  */
 
 // Common types
-export * from './common.types';
+export * from "./common.types";
 
 // Auth types
-export * from './auth.types';
+export * from "./auth.types";
 
 // User types
-export * from './user.types';
+export * from "./user.types";
 
 // Vehicle types
-export * from './vehicle.types';
-export * from './vehicle-profile.types';
+export * from "./vehicle.types";
+export * from "./vehicle-profile.types";
 
 // Supplier types
-export * from './supplier.types';
+export * from "./supplier.types";
 
 // Center types
-export * from './center.types';
+export * from "./center.types";
 
 // Branch types
-export * from './branch.types';
+export * from "./branch.types";
 
 // Category types
-export * from './category.types';
+export * from "./category.types";
 
 // Product types
-export * from './product.types';
+export * from "./product.types";
 
 // Service types
-export * from './service.types';
-export * from './service-type.types';
+export * from "./service.types";
+export * from "./service-type.types";
 
 // Service Package types
-export * from './service-package.types';
+export * from "./service-package.types";
 
 // Catalog types
-export * from './catalog.types';
+export * from "./catalog.types";
 
 // Inventory types
-export * from './inventory.types';
+export * from "./inventory.types";
+
+// Inventory Lot types
+export * from "./inventory-lot.types";
+
+// Stock Transaction types
+export * from "./stock-transaction.types";
 
 // Price Book types
-export * from './price-book.types';
+export * from "./price-book.types";
 
 // Sale Order types
-export * from './sale-order.types';
+export * from "./sale-order.types";
 
 // Service Bay types
-export * from './service-bay.types';
+export * from "./service-bay.types";
 // Purchase Order types
-export * from './purchase-order.types';
+export * from "./purchase-order.types";
 // Promotion types
-export * from './promotion.types';
+export * from "./promotion.types";
 // Promotion Type types
-export * from './promotion-type.types';
+export * from "./promotion-type.types";
 // Booking types
-export * from './booking.types';
+export * from "./booking.types";
 // Service Process types
-export * from './service-process.types';
+export * from "./service-process.types";
 // Service Process Tracking types
-export * from './service-process-tracking.types';
+export * from "./service-process-tracking.types";
 // Media types
-export * from './media.types';
+export * from "./media.types";
 // Product Attribute types
-export * from './product-attribute.types';
+export * from "./product-attribute.types";
 // Payment types
-export * from './payment.types';
+export * from "./payment.types";
